dfaa7b00fe6243c0bd0f95a340b3e6b0e1885e0e
shkong <shkong@DESKTOP-V9QPJM1>
2024-09-26 17:38:13 +0900

    숫자 변경 시 포맷 적용, 기타 연산 개발, 결과값(=) 두번 입력 시 표현 수정중

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,18 +30,24 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private static string NumberFormat(string beforNum)
+        // 입력 숫자 변경 시 숫자포맷 적용
+        private static string NumberFormat(String beforeNum)
         {
+            string[] dotNum;
+            String afterNum;
 
-            if (beforNum.Contains("."))
+            if(beforeNum.Contains("."))
             {
-                string[] dotNum = beforNum.Split(".");
-                beforNum = dotNum[0];
+                dotNum = beforeNum.Split(".");
+                beforeNum = dotNum[0].ToString();
+                afterNum = String.Format("{0:#,##0}", Double.Parse(beforeNum));
+                //afterNum = beforeNum.ToString("N");
+                afterNum = afterNum + "." + dotNum[1].ToString();
             }
-
-            // 형변환 찾아보기
-            String afterNum = String.Format("{0:#,###}", beforNum);
-
+            else
+            {
+                afterNum = String.Format("{0:#,##0}", Double.Parse(beforeNum));
+            }
             return afterNum;
         }
 
@@ -71,8 +77,14 @@ namespace Calculator
             {
                 txtResult.Text += num;
             }
+            // 소수점 입력 시 숫자포맷 적용 함수 타지않고 종료
+            else if (num == ".")
+            {
+                txtResult.Text += num;
+                return;
+            }
 
-            txtResult.Text = NumberFormat(txtResult.Text);
+            txtResult.Text = NumberFormat(txtResult.Text.ToString());
         }
 
         // 사칙연산 클릭
@@ -84,7 +96,7 @@ namespace Calculator
             Button btn = (Button)sender;
             op = btn.Content.ToString();
 
-            txtExp.Text = txtResult.Text + " " + op;
+            txtExp.Text = NumberFormat(saveVaule.ToString()) + " " + op;
         }
 
         // 결과값(=) 클릭
@@ -94,11 +106,13 @@ namespace Calculator
 
             // 결과값 출력 후 또 같은 버튼을 누를경우
             // op 값 확인, 식 빈값으로 안돌아가는거 확인해야
-            if(txtExp.Text.Contains("="))
+            if (txtExp.Text.Contains("="))
             {
-                txtResult.Text += saveVaule;
+                /*txtExp.Text = saveVaule.ToString() + "op" + var1.ToString();*/
+
+/*                txtResult.Text += saveVaule;
                 txtExp.Text = "";
-                txtExp.Text = var1.ToString() + op + saveVaule.ToString();
+                txtExp.Text = var1.ToString() + op + saveVaule.ToString();*/
             }
 
             switch (op)
@@ -126,7 +140,8 @@ namespace Calculator
                     }
                     break;
             }
-            txtExp.Text += " " + var1 + " =";
+            txtResult.Text = NumberFormat(txtResult.Text.ToString());
+            txtExp.Text += " " + NumberFormat(var1.ToString()) + " =";
             OpFlag = true;
         }
 
@@ -136,33 +151,83 @@ namespace Calculator
             Button btn = sender as Button;
             object clear = btn.Content.ToString();
 
-            // 연산자 입력 후 지우기 버튼 눌렀을 경우 지울 수 없음
-            if (OpFlag)
+            if(OpFlag)
             {
-                return;
-            }
-            else
-            {
-                switch (clear)
+                if(!clear.Equals("←"))
                 {
-                    case "←":
-                        if(txtResult.Text.Length == 1 || txtResult.Text == "0")
-                        {
-                            txtResult.Text = "0";
-                        }
-                        else
-                        {
-                            txtResult.Text = txtResult.Text.Remove(txtResult.Text.Length - 1);
-                        }
-                        break;
-                    case "C":
-                        txtResult.Text = "0";
-                        txtExp.Text = "";
-                        break;
-                    case "CE":
-                        txtResult.Text = "0";
-                        break;
+                    txtResult.Text = "0";
+                    return;
                 }
+            }
+            switch (clear)
+            {
+                case "←":
+                    if (txtResult.Text.Length == 1 || txtResult.Text == "0")
+                    {
+                        txtResult.Text = "0";
+                    }
+                    else
+                    {
+                        txtResult.Text = txtResult.Text.Remove(txtResult.Text.Length - 1);
+                    }
+                    break;
+                case "C":
+                    txtResult.Text = "0";
+                    txtExp.Text = "";
+                    break;
+                case "CE":
+                    txtResult.Text = "0";
+                    break;
+            }
+            txtResult.Text = NumberFormat(txtResult.Text.ToString());
+        }
+
+        // ± 클릭
+        private void changeSign(object sender, RoutedEventArgs e)
+        {
+            double changeSign = -double.Parse(txtResult.Text);
+            txtResult.Text = NumberFormat(changeSign.ToString());
+            // 기록이 있을 경우 negate 붙여야함
+        }
+
+        // 제곱
+        private void squareClick(object sender, RoutedEventArgs e)
+        {
+            double square = Math.Pow(double.Parse(txtResult.Text),2);
+            txtExp.Text = "sqr(" + txtResult.Text + ")";
+            txtResult.Text = NumberFormat(square.ToString());
+            OpFlag = true;
+        }
+
+        // 루트
+        private void routeClick(object sender, RoutedEventArgs e)
+        {
+            double route = Math.Sqrt(double.Parse(txtResult.Text));
+            txtExp.Text = "√(" + txtResult.Text + ")";
+            txtResult.Text = NumberFormat(route.ToString());
+            OpFlag = true;
+        }
+
+        private void fractionClick(object sender, RoutedEventArgs e)
+        {
+            txtExp.Text = "1/(" + txtResult.Text + ")";
+            txtResult.Text = NumberFormat((1.0/double.Parse(txtResult.Text)).ToString());
+            OpFlag = true;
+        }
+
+        private void percentClick(object sender, RoutedEventArgs e)
+        {
+           double percent;
+           if(!saveVaule.Equals(""))
+            {
+                percent = double.Parse(txtResult.Text) / 100;
+                txtResult.Text = NumberFormat(percent.ToString());
+                txtExp.Text = txtExp.Text + " " + txtResult.Text;
+            }
+           else
+            {
+                txtResult.Text = "0";
+                txtExp.Text = "0";
             }
         }
     }
